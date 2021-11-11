4c94bc2ec08b3c0c88cd63e50fc520c4a8f6f510
Guilherme <guilhaugusto05@gmail.com>
2021-11-11 18:59:26 -0300

    Requisito 4

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Logo from '../logo.svg';
 import { getUser } from '../services/userAPI';
 import LoadingComp from '../pages/LoadingComp';
@@ -29,15 +30,30 @@ class Header extends Component {
 
     return (
       <header className="header" data-testid="header-component">
-        <img src={ Logo } alt="Logo" />
-        <div className="user-header">
-          <div className="user-icon">
-            <i className="fas fa-circle fa-user" />
+        <div className="header-top">
+          <img src={ Logo } alt="Logo" />
+          <div className="user-header">
+            <div className="user-icon">
+              <i className="fas fa-circle fa-user" />
+            </div>
+            <div className="user-name">
+              {name === ''
+                ? <LoadingComp /> : <p data-testid="header-user-name">{ name }</p>}
+            </div>
           </div>
-          <div className="user-name">
-            {name === ''
-              ? <LoadingComp /> : <p data-testid="header-user-name">{ name }</p>}
-          </div>
+        </div>
+        <div className="header-links">
+          <ul>
+            <li>
+              <Link data-testid="link-to-search" to="/search">Search</Link>
+            </li>
+            <li>
+              <Link data-testid="link-to-favorites" to="/favorites">Favorites</Link>
+            </li>
+            <li>
+              <Link data-testid="link-to-profile" to="/profile">Profile</Link>
+            </li>
+          </ul>
         </div>
       </header>
     );
