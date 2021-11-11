a6e2aa4742569ee1e65c738d85460a6ba135fa0e
Guilherme <guilhaugusto05@gmail.com>
2021-11-11 16:43:00 -0300

    Requisito 2

---
+++ src/pages/LoadingComp.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class LoadingComp extends Component {
+  render() {
+    return (
+      <div className="loading">
+        <h1>Carregando...</h1>
+      </div>
+    );
+  }
+}
+
+export default LoadingComp;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,76 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
+import { createUser } from '../services/userAPI';
+import LoadingComp from './LoadingComp';
 
 class Login extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      userName: '',
+      isSaving: false,
+      isSaved: false,
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.joinClick = this.joinClick.bind(this);
+  }
+
+  handleChange({ target: { name, value } }) {
+    this.setState({ [name]: value });
+  }
+
+  joinClick() {
+    this.setState({ isSaving: true }, () => {
+      const { userName } = this.state;
+
+      createUser({ name: userName })
+        .then(() => {
+          this.setState({
+            isSaved: true,
+            isSaving: false,
+          });
+        });
+    });
+  }
+
   render() {
-    return (<div data-testid="page-login">Login</div>);
+    const { handleChange, joinClick } = this;
+    const { userName, isSaving, isSaved } = this.state;
+
+    const minLength = 3;
+    const canClick = userName.length < minLength;
+
+    const loginPage = (
+      <div data-testid="page-login">
+        <form className="login-form">
+          <input
+            data-testid="login-name-input"
+            placeholder="Nome"
+            type="text"
+            name="userName"
+            value={ userName }
+            onChange={ handleChange }
+          />
+          <button
+            data-testid="login-submit-button"
+            type="button"
+            disabled={ canClick }
+            onClick={ joinClick }
+          >
+            Entrar
+          </button>
+        </form>
+      </div>
+    );
+
+    return (
+      <>
+        {isSaving ? <LoadingComp /> : loginPage}
+        {isSaved && <Redirect to="/search" />}
+      </>
+    );
   }
 }
 
