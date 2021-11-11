79789fdbc262aaf86b4a02597255c160b400b459
Guilherme <guilhaugusto05@gmail.com>
2021-11-11 19:11:31 -0300

    Requisito 5

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,10 +2,50 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 
 class Search extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      artistName: '',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target: { name, value } }) {
+    this.setState({ [name]: value });
+  }
+
   render() {
+    const { handleChange } = this;
+    const { artistName } = this.state;
+
+    const minLength = 2;
+    const isClickable = artistName.length < minLength;
+
     return (
       <div data-testid="page-search">
         <Header />
+        <div className="search-container">
+          <div className="search-input">
+            <input
+              data-testid="search-artist-input"
+              type="text"
+              placeholder="Nome do Artista"
+              name="artistName"
+              value={ artistName }
+              onChange={ handleChange }
+            />
+            <i className="fas fa-search" />
+          </div>
+          <button
+            data-testid="search-artist-button"
+            type="button"
+            disabled={ isClickable }
+          >
+            Search
+          </button>
+        </div>
       </div>
     );
   }
