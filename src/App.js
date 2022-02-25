f61e6164d1e6c86407e667bf4fdb892854d96159
Guilherme <guilhaugusto05@gmail.com>
2022-02-24 22:33:55 -0300

    refractor app and create indexes to pages and components

--- src/App.js
+++ src/App.js
@@ -1,33 +1,13 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { Routes } from './components';
 
-import Login from './pages/Login';
-import Search from './pages/Search';
-import Album from './pages/Album';
-import Favorites from './pages/Favorites';
-import Profile from './pages/Profile';
-import ProfileEdit from './pages/ProfileEdit';
-import NotFound from './pages/NotFound';
-
-class App extends React.Component {
-  render() {
-    return (
-      <BrowserRouter>
-        <Switch>
-          <Route
-            path="/album/:id"
-            render={ (props) => <Album { ...props } /> }
-          />
-          <Route path="/profile/edit" component={ ProfileEdit } />
-          <Route path="/profile" component={ Profile } />
-          <Route path="/favorites" component={ Favorites } />
-          <Route path="/search" component={ Search } />
-          <Route exact path="/" component={ Login } />
-          <Route path="*" component={ NotFound } />
-        </Switch>
-      </BrowserRouter>
-    );
-  }
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+import AlbumResult from './AlbumResult';
+import Header from './Header';
+import MusicCard from './MusicCard';
+import Routes from './Routes';
+
+export { AlbumResult, Header, MusicCard, Routes };
