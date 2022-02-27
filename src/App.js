5dc24f923d112b2395621ccc92a02e57fdadfd3d
Guilherme <guilhaugusto05@gmail.com>
2022-02-27 15:53:57 -0300

    change bootstrap import from index to app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { HashRouter } from 'react-router-dom';
 import { Routes } from './components';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
