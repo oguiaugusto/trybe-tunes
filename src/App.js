c153b4971e19909567c2d2b9f03008b12f5f8eaa
Guilherme <guilhaugusto05@gmail.com>
2022-02-27 00:47:01 -0300

    change browser router to hash router and install gh-pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { Routes } from './components';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes />
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
