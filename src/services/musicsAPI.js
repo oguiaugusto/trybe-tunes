969eab61e98f133184e4eab8d047e142682e45d2
Guilherme <guilhaugusto05@gmail.com>
2022-03-16 15:36:25 -0300

    change musicsAPI request to axios request with headers to fix allow cors problem

--- src/services/musicsAPI.js
+++ src/services/musicsAPI.js
@@ -1,7 +1,21 @@
+const axios = require('axios');
+
+const headers = new Headers();
+
+headers.append('Content-Type', 'application/json');
+headers.append('Accept', 'application/json');
+headers.append('Access-Control-Allow-Origin', 'http://localhost:3000');
+headers.append('Access-Control-Allow-Credentials', 'true');
+headers.append('GET', 'POST', 'OPTIONS');
+// Source: https://stackoverflow.com/questions/43871637/no-access-control-allow-origin-header-is-present-on-the-requested-resource-whe
+
 const getMusics = async (id) => {
-  const request = await fetch(`https://itunes.apple.com/lookup?id=${id}&entity=song`);
-  const requestJson = await request.json();
-  return requestJson.results;
+  const request = await axios(`https://itunes.apple.com/lookup?id=${id}&entity=song`, {
+    credentials: 'include',
+    method: 'POST',
+    headers,
+  });
+  return request.data.results;
 };
 
 export default getMusics;
